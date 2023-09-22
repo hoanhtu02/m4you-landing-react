424c592706a2ae4c8bd4041587a68dfe87cd77f7
hoanhtu02 <hotu.dev@gmail.com>
2023-09-22 22:24:49 +0700

    fix bug override breakoint

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -8,7 +8,7 @@ export default function Survey() {
             name="surveyFormContainer"
             className="w-full mt-[6.875rem] xl:rounded-[1.875rem] bg-tertiary  xl:shadow-md"
         >
-            <div className="md:p-[2rem] sm:p-[1rem]  ssm:p-[2rem_1rem]">
+            <div className="md:p-[2rem] sm:p-[1rem]  p-[2rem_1rem]">
                 <div className="grid lg:grid-cols-[1fr_28.125rem] md:grid-cols-[70%_1fr] sm:grid-cols-[70%_1fr] grid-cols-1 relative">
                     <div>
                         <div className="rounded-full overflow-hidden inline-block">
@@ -24,7 +24,7 @@ export default function Survey() {
                         </p>
                     </div>
                     <img
-                        className="absolute xl:top-[-75%] lg:top-[-50%] md:top-[-75%] md:right-[-4rem]  ssm:right-[-2rem] lg:right-0 lg:w-[15rem] md:w-[16rem] w-[9rem] top-[-5rem] right-0"
+                        className="absolute xl:top-[-75%] lg:top-[-50%] md:top-[-75%] md:right-[-4rem]  right-[-2rem] lg:right-0 lg:w-[15rem] md:w-[16rem] w-[9rem] top-[-5rem] right-0"
                         src="assets/images/flower.png"
                         alt="Flower"
                     />
