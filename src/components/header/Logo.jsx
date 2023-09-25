d81b2c62427d30c0165f5baffd07d78fdeaa7dba
hoanhtu02 <hotu.dev@gmail.com>
2023-09-25 13:21:46 +0700

    change position logo

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -3,7 +3,7 @@ export function Logo() {
         <img
             src="assets/images/logo.jpg"
             alt="M4YOU"
-            className="xl:w-[14rem] lg:w-[14rem] w-[11rem] xl:translate-x-[-26%] md:translate-x-[-6%] translate-x-[-14%]"
+            className="xl:w-[14rem] lg:w-[14rem] w-[11rem] xl:translate-x-[-23%] md:translate-x-[-6%] translate-x-[-14%]"
         />
     );
 }
